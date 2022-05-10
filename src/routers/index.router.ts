c87cb576110b80d142919e784f825c675e42e44b
ruuiisu <luis.gjokaj@gmail.com>
2022-05-10 15:44:32 +0200

    anmeldung admin

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -3,7 +3,6 @@ import express from "express";
 let router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log("test");
   res.redirect("/ticket-erfassen");
 });
 export = router;
