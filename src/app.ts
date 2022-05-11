898c925d40509c765bdf6f03f56a1a3b0c2bb06b
AleFacal <75016125+AleFacal@users.noreply.github.com>
2022-05-11 10:56:57 +0200

    validation 2

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,12 @@ import readRouter from "./routers/read.router";
 import editRouter from "./routers/edit.router";
 import deleteRouter from "./routers/delete.router";
 import loginRouter from "./routers/login.router";
+import {check, validationResult} from "express-validator";
 
 const app = express();
 const port: number = Number(process.env.PORT) || 3000;
 
+const urlencodedparser = bodyParser.urlencoded({ extended: false})
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -26,6 +28,7 @@ app.use('/ticket-erfassen', createRouter);
 app.use('/ticket-anzeigen', readRouter);
 app.use('/edit', editRouter);
 app.use('/delete', deleteRouter);
+
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
